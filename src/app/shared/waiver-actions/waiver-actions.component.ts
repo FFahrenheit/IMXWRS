6bdbc8c3b8466e9670ba325bee36ca4562b6d86e
Iván L. Murillo <diskman199@gmail.com>
2021-05-07 10:47:00 -0500

    Waiver-actions component done

--- src/app/shared/waiver-actions/waiver-actions.component.ts
+++ src/app/shared/waiver-actions/waiver-actions.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, SimpleChanges } from '@angular/core';
 import { Router } from '@angular/router';
 import { User } from 'src/app/models/user.model';
 import { AuthenticationService } from 'src/app/services/authentication.service';
@@ -11,28 +11,30 @@ import { GetWaiverService } from 'src/app/services/get-waiver.service';
 })
 export class WaiverActionsComponent implements OnInit {
 
-  public wr : any;
+  @Input() public wr : any = null;
   public actions = [];
   public id : string;
   public username : string;
 
   constructor(private router : Router,
-              private waiverService : GetWaiverService,
               private authService : AuthenticationService) { }
 
-  ngOnInit(): void {
-    this.wr = this.waiverService.getWaiver();
-    this.id = this.wr.number;
-    this.username = this.authService.getUser().username;
+  ngOnChanges(changes: SimpleChanges): void {
+    if(this.wr != null){
+   
+      this.id = this.wr.number;
+      this.username = this.authService.getUser().username;
+  
+      this.actions.push({
+        action : 'View',
+        link   : ['waivers','view',this.id]
+      });
+      
+      this.loadPossibleActions();
+    }
+  }
 
-    console.log(['Waiver received',this.wr]);
-    
-    this.actions.push({
-      action : 'View',
-      link   : ['waivers','view',this.id]
-    });
-    
-    this.loadPossibleActions();
+  ngOnInit(): void {
   }
 
   /**
@@ -46,12 +48,68 @@ export class WaiverActionsComponent implements OnInit {
    * Reopen waiver * if it's originator
    */
   loadPossibleActions(){
-    return;
+    
+    if(this.wr.originator == this.username){ //Edit, close
+      
+      if(this.wr.status == 'on hold'){
+        
+        this.actions.push({
+          action : 'Edit',
+          link   : ['waivers','edit',this.id]
+        });
+      }else if(this.wr.status == 'open'){
+        
+        this.actions.push({
+          action : 'Close',
+          link   : ['waivers','close',this.id]
+        });
+      }
+      
+      if(this.wr.typeNumber == 4 && this.wr.originator == this.username
+          && (this.wr.status == 'open' || this.wr.status == 'closed')){
 
+            this.actions.push({
+              action : 'Reopen',
+              link   : ['waivers','reopen',this.id]
+            });
+      }
+    }
+
+    this.wr.authorizations.forEach(a =>{
+
+      if(a['manager'] == this.username && a['signed'] == 'pending'){
+        
+        this.actions.push({
+          action : 'Review authorization',
+          link   : ['waivers','authorize',this.id]
+        });
+
+      }
+    });
+
+    this.wr.actions.forEach(a=>{
+      
+      if(a['responsable'] == this.username){
+
+        if(a['signed'] == 'pending'){
+
+          this.actions.push({
+            action : 'Confirm participation',
+            link   : ['waivers','sign',this.id]
+          });
+        }else if(a['signed'] == 'signed' && this.wr.status == 'open'){
+
+          this.actions.push({
+            action : 'Mark activities as done',
+            link   : ['tasks','pending'] 
+          });
+        }
+      }
+    });
   }
 
-  click(){
-    console.log('xd');
+  goTo(link : string[]){
+    this.router.navigate(link);
   }
 
 }
