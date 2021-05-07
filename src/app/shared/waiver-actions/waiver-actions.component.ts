a4b95e8327187b0285aafb34cd4fb5afb019c2d7
Iván L. Murillo <diskman199@gmail.com>
2021-05-07 11:45:04 -0500

    Actions component design improved

--- src/app/shared/waiver-actions/waiver-actions.component.ts
+++ src/app/shared/waiver-actions/waiver-actions.component.ts
@@ -11,30 +11,32 @@ import { GetWaiverService } from 'src/app/services/get-waiver.service';
 })
 export class WaiverActionsComponent implements OnInit {
 
-  @Input() public wr : any = null;
+  @Input() public wr: any = null;
   public actions = [];
-  public id : string;
-  public username : string;
+  public id: string;
+  public username: string;
 
-  constructor(private router : Router,
-              private authService : AuthenticationService) { }
-
-  ngOnChanges(changes: SimpleChanges): void {
-    if(this.wr != null){
-   
-      this.id = this.wr.number;
-      this.username = this.authService.getUser().username;
-  
-      this.actions.push({
-        action : 'View',
-        link   : ['waivers','view',this.id]
-      });
-      
-      this.loadPossibleActions();
-    }
-  }
+  constructor(private router: Router,
+    private authService: AuthenticationService) { }
 
   ngOnInit(): void {
+  }
+  
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+    if (changes.wr.previousValue == undefined && changes.wr.currentValue != null) {
+
+      this.id = this.wr.number;
+      this.username = this.authService.getUser().username;
+
+      this.actions.push({
+        action : 'View',
+        link   : ['waivers', 'view', this.id],
+        icon   : 'far fa-eye'
+      });
+
+      this.loadPossibleActions();
+    }
   }
 
   /**
@@ -47,68 +49,75 @@ export class WaiverActionsComponent implements OnInit {
    * Close waiver * if it's originator
    * Reopen waiver * if it's originator
    */
-  loadPossibleActions(){
-    
-    if(this.wr.originator == this.username){ //Edit, close
-      
-      if(this.wr.status == 'on hold'){
-        
+  loadPossibleActions() {
+
+    if (this.wr.originator == this.username) { //Edit, close
+
+      if (this.wr.status == 'on hold') {
+
         this.actions.push({
           action : 'Edit',
-          link   : ['waivers','edit',this.id]
+          link   : ['waivers', 'edit', this.id],
+          icon   : 'fas fa-pencil-alt'
         });
-      }else if(this.wr.status == 'open'){
-        
+      } else if (this.wr.status == 'open') {
+
         this.actions.push({
           action : 'Close',
-          link   : ['waivers','close',this.id]
+          link   : ['waivers', 'close', this.id],
+          icon   : 'fas fa-check-circle' 
         });
       }
-      
-      if(this.wr.typeNumber == 4 && this.wr.originator == this.username
-          && (this.wr.status == 'open' || this.wr.status == 'closed')){
 
-            this.actions.push({
-              action : 'Reopen',
-              link   : ['waivers','reopen',this.id]
-            });
+      if (this.wr.typeNumber == 4 && this.wr.originator == this.username
+        && (this.wr.status == 'open' || this.wr.status == 'closed')) {
+
+        this.actions.push({
+          action : 'Reopen',
+          link   : ['waivers', 'reopen', this.id],
+          icon   : 'fas fa-sync-alt'
+        });
       }
     }
 
-    this.wr.authorizations.forEach(a =>{
+    this.wr.authorizations.forEach(a => {
 
-      if(a['manager'] == this.username && a['signed'] == 'pending'){
-        
+      if (a['manager'] == this.username && a['signed'] == 'pending') {
+
         this.actions.push({
           action : 'Review authorization',
-          link   : ['waivers','authorize',this.id]
+          link   : ['waivers', 'authorize', this.id],
+          icon   : 'fas fa-marker'
         });
 
       }
     });
 
-    this.wr.actions.forEach(a=>{
-      
-      if(a['responsable'] == this.username){
+    this.wr.actions.forEach(a => {
 
-        if(a['signed'] == 'pending'){
+      if (a['responsable'] == this.username) {
+
+        if (a['signed'] == 'pending') {
 
           this.actions.push({
             action : 'Confirm participation',
-            link   : ['waivers','sign',this.id]
+            link   : ['waivers', 'sign', this.id],
+            icon   : 'fas fa-thumbtack'
           });
-        }else if(a['signed'] == 'signed' && this.wr.status == 'open'){
+        } else if (a['signed'] == 'signed' && this.wr.status == 'open') {
 
           this.actions.push({
             action : 'Mark activities as done',
-            link   : ['tasks','pending'] 
+            link   : ['tasks', 'pending'],
+            icon   : 'fas fa-check-double'
           });
         }
       }
     });
   }
 
-  goTo(link : string[]){
+  goTo(link: string[]) {
+    console.log(link);
     this.router.navigate(link);
   }
 
