a4978646e9f03ff76f0d2202fc6451f47094cdeb
Iván L. Murillo <diskman199@gmail.com>
2021-05-12 07:50:20 -0500

    No stats warning message

--- src/app/shared/error-message/error-message.component.ts
+++ src/app/shared/error-message/error-message.component.ts
@@ -11,12 +11,14 @@ export class ErrorMessageComponent implements OnInit {
   @Input() public title = 'Error';
   @Input() public description = "We couldn't find the requested resource";
   @Input() public type = 'error';
+  @Input() public styles = '';
 
   constructor(public location : Location) { }
   
   public url; 
   public error_url = './../../../assets/img/error.png';
   public empty_url = './../../../assets/img/empty.png';
+  public empty_sub_url = './../../../assets/img/lazy.png';
 
   ngOnInit(): void {
     switch(this.type){
@@ -25,6 +27,9 @@ export class ErrorMessageComponent implements OnInit {
         break;
       case 'empty':
         this.url = this.empty_url;
+        break;
+      case 'empty-sub':
+        this.url = this.empty_sub_url;
         break;
     }
   }
