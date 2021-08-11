ee3f527b37d84abe33603944159171abbc5f105d
Iván L. Murillo <diskman199@gmail.com>
2021-08-11 11:57:41 -0500

    Overall performance progress

--- src/app/stats/overall/overall.component.ts
+++ src/app/stats/overall/overall.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UsersService } from 'src/app/services/users.service';
+import { AlertService } from 'src/app/shared/alert';
+import { Charting } from 'src/app/util/charting';
 
 @Component({
   selector: 'app-overall',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OverallComponent implements OnInit {
 
-  constructor() { }
+  public charts;
+  public user;
+
+  constructor(private alert : AlertService,
+              private userService : UsersService) { }
 
   ngOnInit(): void {
+
+    this.userService.getUser('all').subscribe(
+      resp=>{
+        if(resp == null){
+          this.alert.error("The information of this user couldn't be retrieved");
+        }
+        console.log(resp);
+        if(resp){
+          this.user = this.userService.getCurrentUser();
+          let stats = this.userService.getStats();
+          console.log(stats);
+
+          let data = new Charting();
+          
+          this.charts = data.getCharts(stats, this.user.username);
+          console.log(this.charts);
+        }
+      },
+      error=>{
+        this.alert.error("The information of this user couldn't be retrieved");
+      }
+    )
   }
 
 }
